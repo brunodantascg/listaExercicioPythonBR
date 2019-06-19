4621a50a65ba2f737e07b3d6828904c987d9920f
Bruno Dantas <brunodedantas@gmail.com>
2019-06-19 13:30:25 -0300

    questão 6

---
+++ 1estruturaSequencial/6area.py
@@ -0,0 +1,10 @@
+# Faça um Programa que peça o raio de um círculo, calcule e mostre sua área
+
+# area = pi * raio^2
+
+pi = 3.14
+raio = float(input("Informe raio: "))
+
+area = pi * raio**2
+
+print("A área é {0}".format(area))
---
+++ 1estruturaSequencial/7areaQuadrado.py
@@ -0,0 +1,8 @@
+# Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+
+lado = float(input("Informe valor do lado: "))
+
+area = lado**2
+
+print("A área do quadrado é {0}".format(area))
+print("E o dobro é: ", area*2)
