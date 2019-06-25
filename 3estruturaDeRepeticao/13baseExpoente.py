95805e06361c9c563ffe3518479963699ebc16bc
Bruno Dantas <brunodedantas@gmail.com>
2019-06-25 14:09:34 -0300

    questão 13 e 15

---
+++ 3estruturaDeRepeticao/13baseExpoente.py
@@ -0,0 +1,12 @@
+# Questão 13
+# Faça um programa que peça dois números, base e expoente, calcule e mostre o primeiro número elevado ao segundo número. Não utilize a função de potência da linguagem.
+
+base = int(input("Informe base: "))
+expoente = int(input("Informe expoente: "))
+resultado = 1
+
+for i in range(expoente):
+    resultado = base * base
+    resultado = base * resultado
+
+print(resultado)
