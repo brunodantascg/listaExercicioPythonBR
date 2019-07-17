d3a26cb941b6b956d984d6df44adb498e1906056
Bruno Dantas <brunodedantas@gmail.com>
2019-07-17 16:35:23 -0300

    questão 16

---
+++ 4exerciciosListas/16vendedores.py
@@ -0,0 +1,32 @@
+# Questão 16
+
+# Utilize uma lista para resolver o problema a seguir. Uma empresa paga seus vendedores com base em comissões. O vendedor recebe $200 por semana mais 9 por cento de suas vendas brutas daquela semana. Por exemplo, um vendedor que teve vendas brutas de $3000 em uma semana recebe $200 mais 9 por cento de $3000, ou seja, um total de $470. Escreva um programa (usando um array de contadores) que determine quantos vendedores receberam salários nos seguintes intervalos de valores:
+
+# $200 - $299
+# $300 - $399
+# $400 - $499
+# $500 - $599
+# $600 - $699
+# $700 - $799
+# $800 - $899
+# $900 - $999
+# $1000 em diante
+
+# Desafio: Crie ma fórmula para chegar na posição da lista a partir do salário, sem fazer vários ifs aninhados.
+
+salarioBase = 200
+vendedores = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+for i in range(0, 10):
+    valorVendas = float(input("Informe o valor das vendas do vendedor: "))
+    salario = valorVendas * 0.09 + salarioBase
+    indice = int(salario / 100) - 1
+    if (indice > 9):
+        indice = 9
+    elif (indice < 1):
+        indice = 1
+    print indice
+    vendedores[indice - 1] += 1
+
+for i in range(0, 9):
+    print(" {0} - {1} : {2}".format(i * 100 + 200, (i + 1) * 100 + 199, vendedores[i]))
